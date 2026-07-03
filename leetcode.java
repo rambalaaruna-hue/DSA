30f0b57fa2c5c6bacf2082c2d3d20111dd6a741b
rambalaaruna-hue/pythonfsd <rambalaaruna@gmail.com>
2026-07-03 18:14:06 +0530

    Leetcode #1760:Minimum Limit Of Balls In A Bag

--- leetcode.java
+++ leetcode.java
@@ -1097,44 +1097,87 @@
 
 //Leetcode-1552
 
-import java.util.Arrays;
+// import java.util.Arrays;
 
+// public class leetcode{
+//     public static boolean isPossible(int[] position, int m ,int k){
+//         int prev = position[0];
+//         m -= 1;
+//         for(int i = 1;i < position.length;i++){
+//             int val = position[i];
+//             if(val - prev >= k){
+//                 m -= 1;
+//                 prev = val;
+//             }
+//             if(m <= 0){
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+
+//     public static int maxDistance(int[] position, int m) {
+//         Arrays.sort(position);
+//         int l = 1;
+//         int r = 1000000000;
+//         while( l <= r){
+//             int mid = l + (r - l)/2;
+//             if(isPossible(position,m,mid)){
+//                 l = mid + 1;
+//             }else{
+//                 r = mid - 1;
+//             }
+//         }
+//         return r;
+        
+//     }
+//     public static void main(String[] args) {
+//         int[] position = {1,2,3,4,7};
+//         int m = 3;
+//         System.out.println(maxDistance(position,m));
+        
+//     }
+// }
+
+
+//Leetcode-1760
 public class leetcode{
-    public static boolean isPossible(int[] position, int m ,int k){
-        int prev = position[0];
-        m -= 1;
-        for(int i = 1;i < position.length;i++){
-            int val = position[i];
-            if(val - prev >= k){
-                m -= 1;
-                prev = val;
+    public static boolean isPossible(int[] nums, int maxOperations,int k){
+        for(int i = 0;i < nums.length;i++){
+            int val = nums[i];
+            int temp = val/k;
+            if(val%k != 0){
+                temp++;
             }
-            if(m <= 0){
-                return true;
+            temp--;
+            maxOperations -= temp;
+            if(maxOperations < 0){
+                return false;
             }
         }
-        return false;
+        return true;
     }
 
-    public static int maxDistance(int[] position, int m) {
-        Arrays.sort(position);
+    public static int minimumSize(int[] nums, int maxOperations) {
         int l = 1;
         int r = 1000000000;
-        while( l <= r){
-            int mid = l + (r - l)/2;
-            if(isPossible(position,m,mid)){
-                l = mid + 1;
-            }else{
+        while(l <= r){
+            int mid  = l  + (r - l)/2;
+            if(isPossible(nums,maxOperations,mid)){
                 r = mid - 1;
+            }else{
+                l = mid + 1;
             }
         }
-        return r;
+        return l;
+
         
     }
     public static void main(String[] args) {
-        int[] position = {1,2,3,4,7};
-        int m = 3;
-        System.out.println(maxDistance(position,m));
+        int[] nums = {9};
+        int maxOperations = 2;
+        System.out.println(minimumSize(nums,maxOperations));
+
         
     }
 }
