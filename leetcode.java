2377f22b048ae0c10d51666ca6132910711d77a3
rambalaaruna-hue/pythonfsd <rambalaaruna@gmail.com>
2026-07-04 10:52:49 +0530

    Leetcode #2226:Maximum Candies Allocated To K Children

--- leetcode.java
+++ leetcode.java
@@ -1141,43 +1141,79 @@
 
 
 //Leetcode-1760
-public class leetcode{
-    public static boolean isPossible(int[] nums, int maxOperations,int k){
-        for(int i = 0;i < nums.length;i++){
-            int val = nums[i];
-            int temp = val/k;
-            if(val%k != 0){
-                temp++;
-            }
-            temp--;
-            maxOperations -= temp;
-            if(maxOperations < 0){
-                return false;
-            }
-        }
-        return true;
-    }
+// public class leetcode{
+//     public static boolean isPossible(int[] nums, int maxOperations,int k){
+//         for(int i = 0;i < nums.length;i++){
+//             int val = nums[i];
+//             int temp = val/k;
+//             if(val%k != 0){
+//                 temp++;
+//             }
+//             temp--;
+//             maxOperations -= temp;
+//             if(maxOperations < 0){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
 
-    public static int minimumSize(int[] nums, int maxOperations) {
-        int l = 1;
-        int r = 1000000000;
-        while(l <= r){
-            int mid  = l  + (r - l)/2;
-            if(isPossible(nums,maxOperations,mid)){
-                r = mid - 1;
-            }else{
-                l = mid + 1;
-            }
-        }
-        return l;
+//     public static int minimumSize(int[] nums, int maxOperations) {
+//         int l = 1;
+//         int r = 1000000000;
+//         while(l <= r){
+//             int mid  = l  + (r - l)/2;
+//             if(isPossible(nums,maxOperations,mid)){
+//                 r = mid - 1;
+//             }else{
+//                 l = mid + 1;
+//             }
+//         }
+//         return l;
 
         
+//     }
+//     public static void main(String[] args) {
+//         int[] nums = {9};
+//         int maxOperations = 2;
+//         System.out.println(minimumSize(nums,maxOperations));
+
+        
+//     }
+// }
+
+
+//Leetcode-2226
+public class leetcode{
+    public static  boolean isPossible(int[] candies,long k,int m){
+        for(int  i = 0;i < candies.length;i++){
+            int val = candies[i];
+            int temp = val/m;
+            k -= temp;
+            if(k <= 0){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static int maximumCandies(int[] candies, long k) {
+        int l = 1;
+        int r = 10000000;
+        while(l <= r){
+            int mid = l + (r - l)/2;
+            if(isPossible(candies,k,mid)){
+                l = mid + 1;
+            }else{
+                r = mid - 1;
+            }
+        } 
+        return r;
     }
     public static void main(String[] args) {
-        int[] nums = {9};
-        int maxOperations = 2;
-        System.out.println(minimumSize(nums,maxOperations));
-
+        int[] candies = {5,8,6};
+        long k = 3;
+        System.out.println(maximumCandies(candies, k));
         
     }
 }
