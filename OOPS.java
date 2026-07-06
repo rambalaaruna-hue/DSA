afa192c4229efdd85b07cd716da2a1f187bc2b7e
rambalaaruna-hue/pythonfsd <rambalaaruna@gmail.com>
2026-07-06 11:30:30 +0530

    OOPS:Method Overloading

--- OOPS.java
+++ OOPS.java
@@ -117,69 +117,108 @@
 
 
 //Inheritance
+// class Owner{
+//     String ownerName="Aruna";
+//     int age = 20;
+//     int num = 123;
+//     public void fun(){
+//         System.out.println("This is Parent Class");
+//     }
+
+
+// }
+// class ChaiShop extends Owner{ 
+//      String branchName;
+//      int cupsSold;
+//     int pricePerCup;
+//     public ChaiShop(String branchName,int cupsSold,int pricePerCup){
+//         this.branchName=branchName;
+//         this.cupsSold=cupsSold;
+//         this.pricePerCup=pricePerCup;
+
+//     }
+//     public void order(int n){
+//         cupsSold=n;
+//     }
+//     public void display(){
+//         System.out.println(branchName);
+//         System.out.println(cupsSold);
+//         System.out.println(pricePerCup);
+//         revenue();
+//     }
+//     public void revenue(){
+//         System.out.println("revenue "+(cupsSold)*(pricePerCup));
+//     }
+// }
+
+// class XeroxShop extends Owner{
+//     String branchName;
+//     int papersSold;
+//     int pricePerPaper;
+//     public XeroxShop(String branchName,int papersSold,int pricePerPaper){
+//         this.branchName = branchName;
+//         this.papersSold = papersSold;
+//         this.pricePerPaper = pricePerPaper;
+//     }
+//     public void display(){
+//         System.out.println(branchName);
+//         System.out.println(papersSold);
+//         System.out.println(pricePerPaper);
+//     }
+// }
+
+// public class OOPS {
+//     public static void main(String[] args) {
+//         ChaiShop branch1 = new ChaiShop("Hyd",0,7);
+//         branch1.order(9);
+//          branch1.display();
+//         System.out.println(branch1.num);
+//         branch1.fun();
+
+//         XeroxShop branch2= new XeroxShop("vizag", 5, 2);
+//         branch2.display();
+//          System.out.println(branch2.num);
+//          Owner o=new Owner();
+//          o.fun();
+
+//     }
+    
+// }
+
+
+//Method Overloading
 class Owner{
     String ownerName="Aruna";
     int age = 20;
     int num = 123;
     public void fun(){
-        System.out.println("This is Parent Class");
+        System.out.println(1);
+    }
+
+    public void fun(int a){
+        System.out.println(2);
+    }
+    public void fun(int a,int b){
+        System.out.println(3);
+    }
+    public void fun(int a,String b){
+        System.out.println(4);
+    }
+    public void fun(String a,int b){
+        System.out.println(5);
     }
 
 
-}
-class ChaiShop extends Owner{ 
-     String branchName;
-     int cupsSold;
-    int pricePerCup;
-    public ChaiShop(String branchName,int cupsSold,int pricePerCup){
-        this.branchName=branchName;
-        this.cupsSold=cupsSold;
-        this.pricePerCup=pricePerCup;
-
-    }
-    public void order(int n){
-        cupsSold=n;
-    }
-    public void display(){
-        System.out.println(branchName);
-        System.out.println(cupsSold);
-        System.out.println(pricePerCup);
-        revenue();
-    }
-    public void revenue(){
-        System.out.println("revenue "+(cupsSold)*(pricePerCup));
-    }
-}
-
-class XeroxShop extends Owner{
-    String branchName;
-    int papersSold;
-    int pricePerPaper;
-    public XeroxShop(String branchName,int papersSold,int pricePerPaper){
-        this.branchName = branchName;
-        this.papersSold = papersSold;
-        this.pricePerPaper = pricePerPaper;
-    }
-    public void display(){
-        System.out.println(branchName);
-        System.out.println(papersSold);
-        System.out.println(pricePerPaper);
-    }
 }
 
 public class OOPS {
     public static void main(String[] args) {
-        ChaiShop branch1 = new ChaiShop("Hyd",0,7);
-        branch1.order(9);
-         branch1.display();
-        System.out.println(branch1.num);
-        branch1.fun();
-
-        XeroxShop branch2= new XeroxShop("vizag", 5, 2);
-        branch2.display();
-         System.out.println(branch2.num);
          Owner o=new Owner();
          o.fun();
+         o.fun(5);
+         o.fun(4,8);
+         o.fun(5,"abc");
+         o.fun("aruna",3);
 
     }
     
