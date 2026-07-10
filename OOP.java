08b120efa6fad014139be5dc6c4e7732e5d69243
rambalaaruna-hue/pythonfsd <rambalaaruna@gmail.com>
2026-07-10 19:00:54 +0530

    OOP:Method Overloading and Method Overriding

--- OOP.java
+++ OOP.java
@@ -1,7 +1,10 @@
-//Inheritance
+
+//OOPS:OOP stands for Object-Oriented Programming. It is a programming style (or paradigm) that organizes software design around data, or "objects," rather than functions and logic.
+
+//Inheritance: Inheritance is an OOP mechanism where a subclass acquires the state and behavior of a superclass, establishing an 'IS-A' relationship. In Java, it is implemented using the extends keyword to achieve code reusability and enable runtime polymorphism.
 
 
-//Single Inheritance
+//Single Inheritance:Single inheritance is a type of inheritance where a subclass is derived from only one superclass.
 // class A{
 //     int x = 10;
 //     int y = 20;
@@ -24,7 +27,7 @@
 // }
 
 
-//Multilevel Inheritance
+//Multilevel Inheritance:Multilevel  inheritance is a type of inheritance where a child class acquires properties and behaviour from a parent class which itself inherits from another parent class.
 // class A{
 //     int a = 10;
 //     void addA(){
@@ -54,7 +57,7 @@
 // }
 
 
-//Heirarchical Inheritance
+//Heirarchical Inheritance:Hierarchical inheritance is a structure where multiple subclasses inherit from a single superclass. 
 // class A{
 //     int a = 50;
 //     void disp(){
@@ -86,31 +89,95 @@
 // }
 
 
-//Multiple Inheritance through Interfaces
+//Multiple Inheritance through Interfaces:"Multiple inheritance is a mechanism where one subclass inherits from more than one superclass. While languages like C++ support this directly, Java does not support multiple inheritance with classes to avoid the ambiguity of the Diamond Problem."
   
-interface A{
-   double PI = 3.14;
-}
-interface B{
-    double r = 10.20;
-}
-interface C{
-     public double area();
-}
-class D{
-    void compute(){
-        System.out.println(Math.pow(2,4));
-    }
-}
-class E extends D implements A,B,C{
-    public double area(){
-        return PI*r*r;
-    }
-}
-public class OOP {
-    public static void main(String[] args) {
-         E e = new E();
-         e.compute();
-         System.out.println(e.area());
-}
-}
+// interface A{
+//    double PI = 3.14;
+// }
+// interface B{
+//     double r = 10.20;
+// }
+// interface C{
+//      public double area();
+// }
+// class D{
+//     void compute(){
+//         System.out.println(Math.pow(2,4));
+//     }
+// }
+// class E extends D implements A,B,C{
+//     public double area(){
+//         return PI*r*r;
+//     }
+// }
+// public class OOP {
+//     public static void main(String[] args) {
+//          E e = new E();
+//          e.compute();
+//          System.out.println(e.area());
+// }
+// }
+
+
+//Inner Class
+// class A{
+//     String a = "Aruna";
+//     class B{
+//         String b = "Anjali";
+//     }
+// }
+// public class OOP{
+//     public static void main(String[] args) {
+//         A obja = new A();
+//         System.out.println(obja.a);
+//         A.B objb = obja.new B();
+//         System.out.println(objb.b);
+//     }
+// }
+
+
+//Method Overloading:"Method Overloading is a compile-time polymorphism feature in Java that allows a class to have multiple methods with the exact same name but different parameter lists (signatures)."
+// class A{
+//     void add(int a,int b){
+//            System.out.println("Sum of 2 ints:" +(a+b));
+//     }
+//     void add(int a,int b,int c){
+//         System.out.println("Sum of 3 ints:"+ (a+b+c));
+//     }
+//     void add(double a,double b){
+//         System.out.println("Sum of 2 doubles:"+ (a+b));
+//     }
+// }
+// public class OOP{
+//     public static void main(String[] args) {
+//         A obj = new A();
+//         obj.add(10,20);
+//         obj.add(50,100,150);
+//         obj.add(34.28,97.32);
+//     }
+// }
+
+
+//Method Overriding:Method overriding is an Object-Oriented Programming (OOP) feature where a subclass provides a specific implementation of a method that is already defined in its parent class. When the method is called, the program executes the child class's version instead of the parent's.
+// class A{
+//     void add(int a,int b){
+//            System.out.println("Sum of 2 ints:" +(a+b));
+//     }
+//     void add(int a,int b,int c){
+//         System.out.println("Sum of 3 ints:"+ (a+b+c));
+//     }
+//     void add(double a,double b){
+//         System.out.println("Sum of 2 doubles:"+ (a+b));
+//     }
+// }
+// public class OOP{
+//     public static void main(String[] args) {
+//         A obj = new A();
+//         obj.add(10,20);
+//         obj.add(50,100,150);
+//         obj.add(34.28,97.32);
+//     }
+// }
+
+
+
